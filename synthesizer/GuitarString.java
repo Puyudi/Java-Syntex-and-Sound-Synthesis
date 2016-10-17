5e73db7006a5d750a5a2f6029ed313852112d744
Puyudi <ssxzypyd@gmail.com>
2016-10-17 15:06:08 -0700

    sample method added

--- synthesizer/GuitarString.java
+++ synthesizer/GuitarString.java
@@ -55,6 +55,6 @@ public class GuitarString {
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
